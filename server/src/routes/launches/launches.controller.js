539bd4d193f760ab8def40a2e95f38d64d640752
aakash-301 <aakashshivanshu5@gmail.com>
2022-11-29 18:32:00 +0530

    added Delete lauches api and integrated with front-enbd

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllLaunches, addNewLaunch } = require('../../models/launches.model');
+const { getAllLaunches, addNewLaunch, existLaunchWithId,abortLaunchById } = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
     return res.status(200).json(getAllLaunches());
@@ -25,7 +25,20 @@ function httpAddNewLaunch(req, res) {
     return res.status(201).json(launch);
 }
 
+function httpAbortLaunch(req, res) {
+    const launchId = Number(req.params.id);
+
+    // if launch doesn't exist
+    if (!existLaunchWithId(launchId)) {
+        return res.status(404).json({ error: ' Launch not found' });
+    }
+    // if launch does exist
+    const aborted = abortLaunchById(launchId);
+    return res.status(200).json(aborted);
+}
+
 module.exports = {
     httpGetAllLaunches,
     httpAddNewLaunch,
+    httpAbortLaunch,
 }
