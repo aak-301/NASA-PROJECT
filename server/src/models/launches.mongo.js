740853e9ff74ee26cf8fcbbf0e33988258a842d7
aakash-301 <aakashshivanshu5@gmail.com>
2023-02-19 09:24:10 +0530

    added a launch Shema

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const launchesSchema = new mongoose.Schema({
+
+    flightNumber: { tye: Number, required: true },
+    launchDate: { tye: Date, required: true },
+    mission: { tye: String, required: true },
+    rocket: { tye: String, required: true },
+    target: { type: String, required: true },
+    customers: [String],
+    upcoming: {
+        type: Boolean,
+        required: true
+    },
+    success: {
+        type: Boolean,
+        required: true,
+        default: true
+    },
+});
