07327ee33c65c04c61725fdad4b11ed4ebab7d1f
aakash-301 <aakashshivanshu5@gmail.com>
2023-02-19 09:23:33 +0530

    added mongoose for mongodb connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -6,9 +7,21 @@ const { loadPlanetsata } = require('./models/planets.models');
 
 const PORT = process.env.PORT || 8000;
 
+const mongoURL = "mongodb+srv://aakashshivanshu5:752qWQZRRA7tqcp0@nasa.cgxehw9.mongodb.net/?retryWrites=true&w=majority"
+
 const server = http.createServer(app);
 
+mongoose.connection.on('open', () => {
+    console.log('MongoDB connection ready!');
+})
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+})
+
 async function startServer() {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(mongoURL);
     await loadPlanetsata();
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}`);
