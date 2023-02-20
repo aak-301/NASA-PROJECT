62c27e0b4056312284320810fb8bbb629c32bc4e
aakash-301 <aakashshivanshu5@gmail.com>
2023-02-20 06:03:56 +0530

    removed mongo code from server file

--- server/src/server.js
+++ server/src/server.js
@@ -1,27 +1,15 @@
 const http = require('http');
-const mongoose = require('mongoose');
 
 const app = require('./app');
-
 const { loadPlanetsata } = require('./models/planets.models');
+const {mongoConnect} = require('./services/mongo');
 
 const PORT = process.env.PORT || 8000;
 
-const mongoURL = "mongodb+srv://aakashshivanshu5:752qWQZRRA7tqcp0@nasa.cgxehw9.mongodb.net/?retryWrites=true&w=majority"
-
 const server = http.createServer(app);
 
-mongoose.connection.on('open', () => {
-    console.log('MongoDB connection ready!');
-})
-
-mongoose.connection.on('error', (err) => {
-    console.error(err);
-})
-
 async function startServer() {
-    mongoose.set("strictQuery", false);
-    await mongoose.connect(mongoURL);
+    await mongoConnect();
     await loadPlanetsata();
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}`);
