dae499374ee7dad9105e2e82db4a7fafecf25054
aakash-301 <aakashshivanshu5@gmail.com>
2023-02-20 09:45:16 +0530

    small fix

--- server/src/app.js
+++ server/src/app.js
@@ -11,9 +11,9 @@ app.use(cors({
 }));
 
 app.use(morgan('combined'));
-app.use('/v1', api);
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use('/v1', api);
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 })
